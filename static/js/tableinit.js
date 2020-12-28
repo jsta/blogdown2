0e8eb9e6c9aa524194de98946997d7d797dc6c52
jsta <stachel2@msu.edu>
2020-12-28 10:32:21 -0500

    enable static linking of open science filters, closes #17

--- static/js/tableinit.js
+++ static/js/tableinit.js
@@ -77,29 +77,60 @@ $(document).ready( function () {
         }
     );   
 
-
-    function makeDetailsRow(data){
-        // console.log(data)
-        var text = '<h5>Keywords</h5><p><i>' + (data.keywords || "No details for this package available.") + "</i><p>";
-
-        return '<div class="packagedetails">' + text + '</div>';
-    }
-
-    // Add event listener for opening and closing details
-    $('#packagestable tbody').on('click', 'td.details-control', function () {
-        var tr = $(this).closest('tr');
-        var row = oTable.row( tr );
-        if ( row.child.isShown() ) {
-            $(this).empty().append('<i class="label fa fa-caret-right"></i>')
-            // This row is already open - close it
-            row.child.hide();
-            tr.removeClass('shown');
+        function makeDetailsRow(data){
+            // console.log(data)
+            var text = '<h5>Keywords</h5><p><i>' + (data.keywords || "No details for this package available.") + "</i><p>";
+    
+            return '<div class="packagedetails">' + text + '</div>';
         }
-        else {
-            $(this).empty().append('<i class="label fa fa-caret-down"></i>')
-            // Open this row
-            row.child( makeDetailsRow(row.data())).show();
-            tr.addClass('shown');
+    
+        // Add event listener for opening and closing details
+        $('#packagestable tbody').on('click', 'td.details-control', function () {
+            var tr = $(this).closest('tr');
+            var row = oTable.row( tr );
+            if ( row.child.isShown() ) {
+                $(this).empty().append('<i class="label fa fa-caret-right"></i>')
+                // This row is already open - close it
+                row.child.hide();
+                tr.removeClass('shown');
+            }
+            else {
+                $(this).empty().append('<i class="label fa fa-caret-down"></i>')
+                // Open this row
+                row.child( makeDetailsRow(row.data())).show();
+                tr.addClass('shown');
+            }
+        });
+        
+        var DataTablesLinkify = function(dataTable) {
+        this.dataTable = dataTable;
+        this.url = location.protocol+'//'+location.host+location.pathname;
+        this.link = function() {
+            return this.url +
+                '?dtsearch='+this.dataTable.search() +
+                '&dtpage='+this.dataTable.page();
+                //more params like current sorting column could be added here
         }
-    });
+        //based on http://stackoverflow.com/a/901144/1407478
+        this.getParam = function(name) {
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(location.search);
+            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+        this.restore = function() {
+            var page = this.getParam('dtpage'), 
+                search = this.getParam('dtsearch');
+            if (search) this.dataTable.search(search).draw(false);
+            if (page) this.dataTable.page(parseInt(page)).draw(false);
+            //more params to take care of could be added here
+        }
+        this.restore();
+        return this;
+    };
+
+    linkify = DataTablesLinkify(oTable);
+    var url = linkify.link()
 });
+
+    
